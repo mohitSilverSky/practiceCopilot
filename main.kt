1bc54aca8d8b684b2ad2a8c64f5e0d72ab24858a
Mohit <mohit.silversky@gmail.com>
2025-04-22 11:47:40 +0530

    New commit message

---
+++ main.kt
@@ -0,0 +1,7 @@
+//write a function with main to add two numbers
+fun main() {
+    val num1 = 5
+    val num2 = 10
+    val sum = addNumbers(num1, num2)
+    println("The sum of $num1 and $num2 is $sum")
+}
